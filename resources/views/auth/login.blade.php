bdb6d38af52b513b12743c5ea56a074afda7eb46
8icey <wbnsnbzthzlq@gmail.com>
2024-06-29 17:28:20 +0100

    added pwa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+  <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Tutorial of Laravel Package">
     <title>Login</title>
+ 
+<link rel="apple-touch-icon" href="{{ asset('telecom.png') }}">
+<link rel="manifest" href="{{ asset('/manifest.json') }}">
+    <script src="{{ asset('/sw.js') }}"></script>
+
     
 </head>
 <link rel="icon" type="image/png" href="IMAGES/favicon.png">
@@ -337,6 +343,22 @@ body::before {
     </svg> 
   
     </svg>
+    <script>
+      if ("serviceWorker" in navigator) {
+         // Register a service worker hosted at the root of the
+         // site using the default scope.
+         navigator.serviceWorker.register("/sw.js").then(
+         (registration) => {
+            console.log("Service worker registration succeeded:", registration);
+         },
+         (error) => {
+            console.error(`Service worker registration failed: ${error}`);
+         },
+       );
+     } else {
+        console.error("Service workers are not supported.");
+     }
+   </script>
 </body>
   
 </html>
